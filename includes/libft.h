1eaeb6c648aa7be1c307813d9061e93ec38d5edb
Tanvir <tanvir@laptop>
2023-07-27 01:42:36 +0400

    made code cleaner

--- includes/libft.h
+++ includes/libft.h
@@ -6,24 +6,27 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 12:35:15 by tanas             #+#    #+#             */
-/*   Updated: 2023/01/08 15:25:53 by tanas            ###   ########.fr       */
+/*   Updated: 2023/07/27 01:41:41 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdarg.h>
 # include <stdint.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "ft_printf.h"
-# include "get_next_line.h"
 
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
 
 // ---------------------- IS-UTILS ---------------------------------------
 int		ft_isalnum(int c);
@@ -51,6 +54,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_calloc(size_t count, size_t size);
+void	free_double_ptr(void **double_ptr);
 
 // ------------------------ PUT-UTILS ------------------------------------
 void	ft_putchar_fd(char c, int fd);
@@ -79,5 +83,20 @@ int		ft_atoi(const char *str);
 char	*ft_itoa(int n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
+// ---------------------- GET-NEXT-LINE ----------------------------------
+char	*get_next_line(int fd);
+char	*get_line(char *str);
+char	*read_file(int fd, char *stash);
+char	*update_stash(char *old_stash);
+
+// ---------------------- FT-PRINTF --------------------------------------
+int		ft_printf(const char *str, ...);
+int		ft_print_char(const char c);
+int		ft_print_string(char *str);
+int		ft_print_number(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int n, char *base);
+int		ft_print_pointer(size_t pointer, char *base);
 
 #endif
